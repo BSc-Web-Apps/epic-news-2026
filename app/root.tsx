5ed64691f9cd46cfa910927f4f0f67a09898dea5
GedSargent <120935232+GedSargent@users.noreply.github.com>
2025-09-18 10:47:51 +0100

    Completed epic-news/ui/add-navbar-and-footer

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import Document from './components/shared-layout/Document.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
+import HeaderWithSearch from './components/organisms/HeaderWithSearch'
+import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
 
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
@@ -21,6 +23,8 @@ export default function App() {
 	return (
 		<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
 			<div className="flex h-screen flex-col justify-between">
+				<HeaderWithSearch />
+
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega">Welcome to Epic News!</h1>
@@ -30,6 +34,8 @@ export default function App() {
 				<div className="container flex justify-between pb-5">
 					<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
 				</div>
+
+				<FooterMenuRight />
 			</div>
 		</Document>
 	)
