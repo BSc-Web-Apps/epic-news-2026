f79e7e375520c8c3e562173a82b7202632313ccc
GedSargent <120935232+GedSargent@users.noreply.github.com>
2025-09-27 20:46:51 +0100

    Completed epic-news/ui/adding-links

--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -1,4 +1,4 @@
-import { useMatches, Link } from 'react-router'
+import { Link, useMatches } from 'react-router'
 import { SearchBar } from '#app/components/search-bar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { UserDropdown } from '#app/components/user-dropdown.tsx'
@@ -14,10 +14,18 @@ export default function HeaderWithSearch() {
 	return (
 		<header className="container py-6">
 			<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-				<div className="flex items-center gap-4">
-					<img src={logo} alt="Epic News Logo" className="w-16" />
-					<span className="text-foreground text-sm">Epic News</span>
-				</div>
+				<Link to="/" className="flex items-center gap-4">
+					<div className="flex items-center gap-4">
+						<img src={logo} alt="Epic News Logo" className="w-16" />
+						<span className="text-foreground text-sm">Epic News</span>
+					</div>
+				</Link>
+				<Link
+					to="/news"
+					className="text-muted-foreground hover:text-foreground text-sm font-semibold transition"
+				>
+					News
+				</Link>
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
 					{searchBar}
 				</div>
