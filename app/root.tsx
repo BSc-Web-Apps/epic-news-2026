e071c44f95eee6e0bcce1b31ddf3101e371b06e5
GedSargent <120935232+GedSargent@users.noreply.github.com>
2025-09-29 06:34:22 +0100

    Completed epic-news/routing/dynamic-routes

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,14 @@
-import { Link, Outlet, useLoaderData } from 'react-router'
+import { Outlet, useLoaderData } from 'react-router'
 import { ParallaxProvider } from 'react-scroll-parallax'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import FooterMenuRight from './components/organisms/Footer/FooterMenuRight'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch'
-import ParallaxBackground from './components/organisms/Hero/ParallaxBackground.tsx'
 import Document from './components/shared-layout/Document.tsx'
-import { Button } from './components/ui/button.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
-import heroImage from '~/assets/jpg/sample-hero.jpg'
-import logo from '~/assets/svg/epic-news-sample-logo.svg'
 
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
@@ -32,29 +28,6 @@ export default function App() {
 					<HeaderWithSearch />
 
 					<div className="flex-1">
-						{/* <main className="h-full">
-							<ParallaxBackground
-								image={heroImage}
-								title="Epic News"
-								logo={logo}
-								altText="Welcome to Epic News, where the latest developments in tech are found."
-							>
-								<div className="bg-secondary/40 mx-auto flex w-fit flex-1 flex-col justify-between gap-16 px-28 py-16 backdrop-blur-sm">
-									<p className="text-secondary-foreground text-center text-4xl font-extrabold">
-										The latest tech news in one place
-									</p>
-									<div className="flex justify-center gap-8">
-										<Button variant="default" size="wide">
-											<Link to="/signup">Sign up</Link>
-										</Button>
-										<Button variant="secondary" size="wide">
-											<Link to="/login">Login</Link>
-										</Button>
-									</div>
-								</div>
-							</ParallaxBackground>
-						</main> */}
-
 						<Outlet />
 					</div>
 
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,34 @@
-import { type MetaFunction } from 'react-router'
+import { Link, type MetaFunction } from 'react-router'
+import ParallaxBackground from '#app/components/organisms/Hero/ParallaxBackground.tsx'
+import { Button } from '#app/components/ui/button.tsx'
+import heroImage from '~/assets/jpg/sample-hero.jpg'
+import logo from '~/assets/svg/epic-news-sample-logo.svg'
 
 export const meta: MetaFunction = () => [{ title: 'Epic News' }]
 
 export default function Index() {
 	return (
-		<main className="grid h-full place-items-center">
-			<h1 className="text-mega">
-				Hello from{' '}
-				<pre className="prose bg-primary text-primary-foreground rounded-lg p-6">
-					app/routes/_index.tsx
-				</pre>
-			</h1>
+		<main className="h-full">
+			<ParallaxBackground
+				image={heroImage}
+				title="Epic News"
+				logo={logo}
+				altText="Welcome to Epic News, where the latest developments in tech are found."
+			>
+				<div className="bg-secondary/40 mx-auto flex w-fit flex-1 flex-col justify-between gap-16 px-28 py-16 backdrop-blur-sm">
+					<p className="text-secondary-foreground text-center text-4xl font-extrabold">
+						The latest tech news in one place
+					</p>
+					<div className="flex justify-center gap-8">
+						<Button variant="default" size="wide">
+							<Link to="/signup">Sign up</Link>
+						</Button>
+						<Button variant="secondary" size="wide">
+							<Link to="/login">Login</Link>
+						</Button>
+					</div>
+				</div>
+			</ParallaxBackground>
 		</main>
 	)
 }
