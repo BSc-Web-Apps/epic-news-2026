61673f70bc5b6fd9379bb4e94745dcfb8107d368
GedSargent <120935232+GedSargent@users.noreply.github.com>
2025-10-16 13:09:44 +0100

    Completed epic-news/displaying-articles/filter-articles-by-category

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,29 @@
-import { Link, type MetaFunction } from 'react-router'
+import { data, Link, type MetaFunction, useLoaderData } from 'react-router'
 import ParallaxBackground from '#app/components/organisms/Hero/ParallaxBackground.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import heroImage from '~/assets/jpg/sample-hero.jpg'
 import logo from '~/assets/svg/epic-news-sample-logo.svg'
+import ArticleCard from '~/components/organisms/ArticleCard.tsx'
+import { prisma } from '~/utils/db.server.ts'
 
 export const meta: MetaFunction = () => [{ title: 'Epic News' }]
 
+export async function loader() {
+	const allArticles = await prisma.article.findMany({
+		select: {
+			id: true,
+			title: true,
+			category: { select: { name: true } },
+			images: { select: { objectKey: true } },
+		},
+	})
+
+	return data({ allArticles })
+}
+
 export default function Index() {
+	const { allArticles } = useLoaderData<typeof loader>()
+
 	return (
 		<main className="h-full">
 			<ParallaxBackground
@@ -29,6 +46,24 @@ export default function Index() {
 					</div>
 				</div>
 			</ParallaxBackground>
+			<div className="container py-16">
+				<h2 className="text-h2 mb-8 font-normal">Latest news</h2>
+
+				<div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+					{allArticles.length > 0 ? (
+						allArticles.map((article) => (
+							<ArticleCard
+								key={article.id}
+								title={article.title}
+								category={article.category?.name}
+								objectKey={article.images[0]?.objectKey}
+							/>
+						))
+					) : (
+						<p>No articles found</p>
+					)}
+				</div>
+			</div>
 		</main>
 	)
 }
