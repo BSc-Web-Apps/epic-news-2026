d263783fc5e5c40183bd660916f9522219418fcc
GedSargent <120935232+GedSargent@users.noreply.github.com>
2025-10-20 06:59:59 +0100

    Completed epic-news/admin-page/admin-only-links

--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -2,7 +2,7 @@ import { Link, useMatches } from 'react-router'
 import { SearchBar } from '#app/components/search-bar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { UserDropdown } from '#app/components/user-dropdown.tsx'
-import { useOptionalUser } from '#app/utils/user.ts'
+import { useOptionalUser, userHasRole } from '#app/utils/user.ts'
 import logo from '~/assets/svg/epic-news-sample-logo.svg'
 
 export default function HeaderWithSearch() {
@@ -10,6 +10,8 @@ export default function HeaderWithSearch() {
 	const isOnSearchPage = matches.find((m) => m.id === 'routes/users+/index')
 	const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
 	const user = useOptionalUser()
+	console.log({ user, userRoles: user?.roles })
+	const isAdminUser = user ? userHasRole(user, 'admin') : false
 
 	return (
 		<header className="container py-6">
@@ -21,7 +23,15 @@ export default function HeaderWithSearch() {
 					</div>
 				</Link>
 
-				<div className="flex flex-1 justify-center gap-8">
+				<div className="flex flex-1 items-center justify-center gap-8">
+					{isAdminUser && (
+						<Link
+							to="/admin-review"
+							className="text-foreground rounded-lg bg-green-900 px-4 py-2 text-sm font-semibold transition hover:bg-green-800"
+						>
+							Admin Review
+						</Link>
+					)}
 					<Link
 						to="/news"
 						className="text-muted-foreground hover:text-foreground text-sm font-semibold transition"
